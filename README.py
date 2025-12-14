abeb72b3aec7114776ea8ba6348e9e8c7c27b38e
ASRAR AHMED <sr.asrar05@gmail.com>
2025-12-14 23:25:10 +0530

    Remove QR code generation example
    Removed QR code generation code from README.py

--- README.py
+++ README.py
@@ -1,10 +1,1 @@
-import qrcode
 
-url = input('Enter a URL:').strip()
-fp = 'C:\\Users\DELL\\OneDrive\\Desktop\\qrcode.png'
-
-qr = qrcode.QRCode()
-qr.add_data(url)
-
-img = qr.make_image()
-img.save(fp)
